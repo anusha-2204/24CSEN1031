cee2e4ff189f4d48adf0156a2a098d45a8be9244
anusha-2204 <akandreg@student.gitam.edu>
2024-10-04 10:59:21 +0530

    Create Format specifiers .c
    Format specifiers

---
+++ Format specifiers .c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int a = 15;
+    printf("Decimal representation: %d \nOctal representation: %o\nHexademical representation: %x", a, a, a);
+
+    return 0;
+}
